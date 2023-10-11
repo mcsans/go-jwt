279cf36bef270e0919ae179066bf00b1d78707c8
mcsans <mihsansaepulloh9@gmail.com>
2023-10-11 13:37:08 +0700

    Finish Add JWT, Middleware, Create Product Bagian 1

--- router/router.go
+++ router/router.go
@@ -3,16 +3,22 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/mcsans/go-jwt/controllers"
+	"github.com/mcsans/go-jwt/middlewares"
 )
 
 func StartApp() *gin.Engine {
 	r := gin.Default()
 
 	userRouter := r.Group("/users")
-
 	{
 		userRouter.POST("/register", controllers.UserRegister)
 		userRouter.POST("/login", controllers.UserLogin)
+	}
+
+	productRouter := r.Group("/products")
+	{
+		productRouter.Use(middlewares.Authentication())
+		productRouter.POST("/", controllers.CreateProduct)
 	}
 
 	return r
